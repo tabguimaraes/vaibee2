b7602f7db614315efd5fe5fd38b204fb8224889c
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-09-17 16:50:37 -0300

    testes com o fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 console.log("ok");
+
+const imgPop = document.getElementById("imgPop");
+
+const url = `https://api.deezer.com/album/302127`;
+
+async function getData() {
+  const response = await fetch(url);
+  let responseData = await response.json();
+  console.log(responseData);
+
+  imgPop.src = responseData.cover_medium;
+}
+
+// getData();
