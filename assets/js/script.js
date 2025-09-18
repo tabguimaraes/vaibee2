23c12d5ce470a0b7b604cf6e8160536149579f1c
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-09-18 14:55:16 -0300

    ajustes de interface do container filho

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,15 @@ console.log("ok");
 
 const imgPop = document.getElementById("imgPop");
 
-const url = `https://api.deezer.com/album/302127`;
+// URL principal para albuns
+const url = `https://api.deezer.com/chart/`;
 
 async function getData() {
   const response = await fetch(url);
   let responseData = await response.json();
   console.log(responseData);
 
-  imgPop.src = responseData.cover_medium;
+  // imgPop.src = responseData.cover_medium;
 }
 
 getData();
